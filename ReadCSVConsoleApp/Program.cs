7c62a0a6ccaff17b4864b6cd9cc9826c2e26517e
igame3dbill <bill@igame3d.com>
2018-04-09 22:44:46 -0600

    ReadCSVConsoleApp initial upload

---
+++ ReadCSVConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace ReadCSVConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] records = File.ReadAllLines("Data.csv");
+            while (true)
+            {
+                Console.Write("Enter first field or 'q' to quit : ");
+                string first = Console.ReadLine();
+                if (first == "q" || first == "Q") return;
+                bool found = false;
+                foreach (string record in records)
+                {
+                    if (record.StartsWith(first + ","))
+                    {
+                        string[] fields = record.Split(',');
+                        int i = 0;
+                        foreach (string content in fields)
+                        {
+                            Console.WriteLine("Field " + i + " is : {0}", fields[i]);
+                            i++;
+                        }
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) Console.WriteLine("There is no record with a first field of {0}", first);
+                Console.WriteLine();
+            }
+        }
+    }
+}
